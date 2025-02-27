47ccc2955419d48805e6fdf99ca447736c41e182
maria parma <mariaparma.tds1.senai@gmail.com>
2025-02-27 10:29:36 -0300

    feat : create Card, Header  and footer

---
+++ my-app/app/page.jsx
@@ -0,0 +1,80 @@
+import  Header from "../components/Header"
+import Card from "../components/Card"
+import Footer from "@/components/Footer";
+export default function Home () {
+  return(
+  <div>
+    <Header/>
+    <main>
+      <h2> Minha Primeira Página NextJs</h2>
+    </main>
+
+    <aside>
+    <Card title="Dona" content="Tem segunda intenção nesse: Não, não para, não
+No selinho no fim do beijo que você insiste em me dar
+Isso é coisa de quem quer amar
+Cê me conheceu depois que deixei de ser dois e voltei a ser só um
+Você não tem culpa, isso é um erro comum
+
+Eu tava ali pra esquecer alguém
+Cê tava ali pra encontrar alguém
+E encontrou, só que tem um porém
+
+O coração que cê quer entrar tem dona
+O te amo que cê quer escutar tem dona
+Eu posso te emprestar meu corpo porque ele ainda é meu
+Mas o sentimento ela não me devolveu
+
+O coração que cê quer entrar tem dona
+O te amo que cê quer escutar tem dona
+Eu posso te emprestar meu corpo porque ele ainda é meu
+Mas o sentimento ela não me devolveu
+Ela não me devolveu" />
+
+    <Card title="Violão"content="Fiquei sabendo
+Que tá esperando um bebê
+Sempre foi seu sonho, eu lembro
+Fico tão feliz por você
+
+Sabe, de vez em quando ainda vejo suas fotos
+Mudou, eu sei
+Mas tem o mesmo olhar que tinha aos dezesseis
+
+Só queria saber
+Você sente o quê
+Quando ouve a minha voz
+No rádio ou na TV?
+
+Então, me tira uma dúvida
+Eu sei que a saudade de nós dois já não machuca
+Mas o que você vai dizer
+Quando seu filho aprender
+Violão com as minhas músicas?" />
+
+    <Card title=" Chova ou Choro" content="Meus amigos chamando pra sair
+Eu caçando motivo pra ficar
+O tempo tava armando pra chover
+A desculpa eu já tinha, mas fui lá
+
+Porque o medo real era ver quem eu gosto em outra boca
+Mas o evento era grande, tinha não sei quantas mil pessoas
+A chance de dar ruim era de 0,001
+Mas quando eu olho pro lado, hum
+
+Bem na hora que eu vi ela beijando outro
+Pra minha sorte, cai um toró, me molha todo
+Ela nunca vai saber o que molhou meu rosto
+Se foi chuva ou choro, chuva ou choro
+
+Bem na hora que eu vi ela beijando outro
+Pra minha sorte, cai um toró, me molha todo
+Ela nunca vai saber o que molhou meu rosto
+Se foi chuva ou choro, chuva ou choro
+
+"/>
+</aside>
+
+    <Footer/>
+  </div>
+  );
+}    
---
+++ my-app/components/Footer.jsx
@@ -0,0 +1,7 @@
+import styles from '../styles/Footer.module.css';
+export default function Footer () {
+    return(
+        <h1 className={styles.footer}> DS</h1> 
+    );
+    
+}
